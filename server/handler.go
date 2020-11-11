473ceb75ccdcf727ffbc740b3faac95827783976
Phusita Sanguansethakul <phusita.dev@gmail.com>
2020-11-11 20:52:50 +0000

    feat: add handlers for spotify login and callback

--- server/handler.go
+++ server/handler.go
@@ -1,21 +1,62 @@
 package server
 
 import (
-	"log"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	errorInvalidSpotifyAuthCode  = errors.New("invalid spotify authorization code")
+	errorInvalidSpotifyAuthState = errors.New("invalid spotify auth state")
+	errorUnableToGetCookie       = errors.New("unable to get cookie")
+	errorUnableLogIn             = errors.New("unable to login to spotify")
+
+	defaultCookieExpires = 60
 )
 
 type Handler struct {
 	botClient *linebot.Client
+	spotify   SpotifyService
 }
 
-func NewHandler(b *linebot.Client) Handler {
+func NewHandler(b *linebot.Client, s SpotifyService) Handler {
 	return Handler{
 		botClient: b,
+		spotify:   s,
 	}
+}
+
+func RandStringBytesMaskImprSrcSB(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const (
+		letterIdxBits = 6
+		letterIdxMask = 1<<letterIdxBits - 1
+		letterIdxMax  = 63 / letterIdxBits
+	)
+
+	var src = rand.NewSource(time.Now().UnixNano())
+	sb := strings.Builder{}
+	sb.Grow(n)
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			sb.WriteByte(letterBytes[idx])
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return sb.String()
 }
 
 func (h *Handler) returnError(err error) error {
@@ -45,10 +86,56 @@ func (h *Handler) Callback(c echo.Context) error {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 				if _, err = h.botClient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-					log.Print(err)
+					log.Err(err)
 				}
 			}
 		}
+	}
+
+	return c.JSON(http.StatusOK, "")
+}
+
+func (h *Handler) SignUp(c echo.Context) error {
+	state := RandStringBytesMaskImprSrcSB(16)
+
+	cookie := http.Cookie{
+		Name:    AuthState,
+		Value:   state,
+		Expires: time.Unix(int64(defaultCookieExpires), 0),
+	}
+	c.SetCookie(&cookie)
+
+	err := h.spotify.Login(state)
+	if err != nil {
+		return h.returnError(errors.Wrap(err, "[SignUp]: unable to sign up"))
+	}
+
+	return c.JSON(http.StatusOK, "")
+}
+
+func (h *Handler) SpotifyLoginCallback(c echo.Context) error {
+	errParam := c.QueryParam("error")
+	if errParam != "" {
+		return h.returnError(errors.Wrapf(errorUnableLogIn, "[SpotifyLoginCallback]: unable to login to spotify due to %v", errParam))
+	}
+
+	code := c.QueryParam("code")
+	if code == "" {
+		return h.returnError(errorInvalidSpotifyAuthCode)
+	}
+
+	state := c.QueryParam("state")
+	if state == "" {
+		return h.returnError(errorInvalidSpotifyAuthState)
+	}
+
+	storedState, err := c.Cookie(AuthState)
+	if err != nil {
+		return h.returnError(errorUnableToGetCookie)
+	}
+
+	if state != storedState.Value {
+		return h.returnError(errorInvalidSpotifyAuthState)
 	}
 
 	return c.JSON(http.StatusOK, "")

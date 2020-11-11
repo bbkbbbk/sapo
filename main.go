9d1602d0fe7fa5023f8a841715f882397557b39f
Phusita Sanguansethakul <phusita.dev@gmail.com>
2020-11-11 20:52:59 +0000

    feat: inject dependencies

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/labstack/echo"
 	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/rs/zerolog/log"
+	"go.mongodb.org/mongo-driver/mongo"
 
+	pkgMongo "github.com/bbkbbbk/sapo/pkg/mongo"
 	"github.com/bbkbbbk/sapo/server"
 )
 
 var (
-	bot *linebot.Client
-	err error
+	bot     *linebot.Client
+	db      *mongo.Database
+	spotify server.SpotifyService
+	err     error
 )
 
 func init() {
@@ -21,14 +25,30 @@ func init() {
 		os.Getenv("CHANNEL_TOKEN"),
 	)
 	if err != nil {
-		log.Fatal(err)
+		log.Err(err)
 	}
+}
+
+func init() {
+	db = pkgMongo.NewMongo(pkgMongo.Config{
+		AuthSource: os.Getenv("MONGO_AUTH_SOURCE"),
+		Database:   os.Getenv("MONGO_DATABASE"),
+		Host:       os.Getenv("MONGO_HOST"),
+		Username:   os.Getenv("MONGO_USERNAME"),
+		Password:   os.Getenv("MONGO_PASSWORD"),
+	})
 }
 
 func main() {
 	e := echo.New()
 
-	serverHandler := server.NewHandler(bot)
+	serverHandler := server.NewHandler(bot, spotify)
+	repository := server.NewRepository(db)
+	spotify = server.NewSpotifyService(
+		os.Getenv("MY_CLIENT_ID"),
+		os.Getenv("MY_CLIENT_SECRET"),
+		repository,
+	)
 
 	e.GET("/", serverHandler.HomePage)
 	e.GET("/ping", serverHandler.PingCheck)
